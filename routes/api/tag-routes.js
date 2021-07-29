3fdb26d2b5b21115c9db92dda77d347408251575
Bipin <bipp2009@gmail.com>
2021-07-29 19:54:30 +0100

    find all by tag code added

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,11 +1,21 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const { Tag, Product, ProductTag, Category } = require('../../models');
 
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+Tag.findAll({
+  include: [{
+    model: Product,
+    through: ProductTag
+  }
+  ]
+}).then((tag)=>{
+  res.json(tag)
+})  // be sure to include its associated Product data
+
+
 });
 
 router.get('/:id', (req, res) => {
