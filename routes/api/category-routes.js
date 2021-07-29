9ee61bb3f2271b5d5f8e9b82849aaeebaf396ab2
Bipin <bipp2009@gmail.com>
2021-07-29 19:12:01 +0100

    all categories now ready

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,7 +4,6 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-
   // find all categories
   // be sure to include its associated Products
   Category.findAll({
@@ -17,18 +16,43 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findAll({
+    where: {
+      id: req.params.id
+    },
+    include: [Product]
+  }).then((cats) => {
+    res.json(cats)
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body).then((cat) => {
+    res.json(cat)
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    }
+  }).then((cat) => {
+    res.json(cat)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then((cat) => {
+    res.json(cat)
+  })
 });
 
 module.exports = router;
