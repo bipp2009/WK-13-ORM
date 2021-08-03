5dc6eb907f7a2960423867458350ff5e2564855f
Bipin <bipp2009@gmail.com>
2021-08-03 18:36:50 +0100

    product requests completed

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -20,8 +20,7 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-  tag
-    .findAll({
+  Tag.findAll({
       where: {
         id: req.params.id,
       },
